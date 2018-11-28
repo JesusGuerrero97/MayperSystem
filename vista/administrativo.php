67e55e61e188040e14beed6fdaed3047bac2d6b7
JesusGuerrero97 <32663331+JesusGuerrero97@users.noreply.github.com>
2018-11-28 09:35:36 -0700

    primer commit

---
+++ vista/administrativo.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Administrativo </title>
+    <link rel="stylesheet" href="../public  /css/estilo.css">
+    <link rel="stylesheet" href="../public/css/modal.css">
+  </head>
+  <body>
+    <div id="sechead">
+      <div class="logoPrincipal"><img width="250px" height="auto" src="../public/img/logo.png" alt="logo Mayper"></div>
+      <div class="in ce"><img class="logo" src="../Public/img/aprovaciones.png" alt=""><p class="vi">Aprobaciones</p></div>
+      <div class="in"><img class="logo" src="../Public/img/equipos.png" alt=""><p class="vi">Productos</p></div>
+      <div class="information">
+        <img  class="perfil" src="../Public/img/user.png" alt="usuario">
+        <p class="r">Jesus Guadalupe Guerrero Partida <br>Puesto: Programador</p>
+      </div>
+    </div>
+    <div id="noti">
+      <div class="cont">
+        <div class="notification-pane" id="mynotification">
+           <figure>
+                <img class="notification_image" src="../Public/img/notification_icon.png" alt="notificacion" title="Notificación">
+           </figure>
+       </div>
+       <div id="myModal" class="modal">
+         <!-- Modal content -->
+         <div class="modal-content">
+           <div class="modal-header">
+             <span class="close">&times;</span>
+             <h2>Notificaciones</h2>
+           </div>
+           <div class="modal-body">
+             <p>Some text in the Modal Body</p>
+             <p>Some other text...</p>
+           </div>
+         </div>
+       </div>
+       <div class="exit">
+          <figure>
+               <img class="notification_image" src="../Public/img/exit.png" alt="">
+          </figure>
+      </div>
+        <!--<div class="ot">
+          Notificaciones
+        </div>-->
+      </div>
+    </div>
+    <div id="body">
+      Hola Como estas
+    </div>
+    <div class="body2">
+    </div>
+  </body>
+  <script src="../Public/js/modal.js"></script>
+</html>
---
+++ vista/inventario.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Inventario</title>
+    <link rel="stylesheet" href="../public/css/estilo.css">
+    <link rel="stylesheet" href="../public/css/modal.css">
+  </head>
+  <body>
+    <div id="sechead">
+      <div class="logoPrincipal"><img width="250px" height="auto" src="../public/img/logo.png" alt="logo Mayper"></div>
+      <div class="in inprincipal"><img class="logo" src="../Public/img/equipos.png" alt=""><p class="vi">Equipos</p></div>
+      <div class="in"><img class="logo" src="../Public/img/ventas.png" alt=""><p class="vi">Ventas</p></div>
+      <div class="in"><img class="logo" src="../Public/img/Demo.png" alt=""><p class="vi">Demos</p></div>
+      <div class="in"><img class="logo" src="../Public/img/Reportes.png" alt=""><p class="vi">Reportes</p></div>
+      <div class="in"><img class="logo" src="../Public/img/config.png" alt=""><p class="vi">Configuración</p></div>
+      <div class="in"><img class="logo" src="../Public/img/respaldo.png" alt=""><p class="vi">Respaldo</p></div>
+      <div class="information">
+        <img  class="perfil" src="../Public/img/user.png" alt="usuario">
+        <p class="r">Jesus Guadalupe Guerrero Partida <br>Puesto: Programador</p>
+      </div>
+    </div>
+    <div id="noti">
+        <div class="notification-pane" id="mynotification">
+           <figure>
+                <img class="notification_image" src="../Public/img/notification_icon.png" alt="notificacion" title="Notificación">
+           </figure>
+       </div>
+       <div id="myModal" class="modal">
+         <!-- Modal content -->
+         <div class="modal-content">
+           <div class="modal-header">
+             <span class="close">&times;</span>
+             <h2>Notificaciones</h2>
+           </div>
+           <div class="modal-body">
+             <p>Some text in the Modal Body</p>
+             <p>Some other text...</p>
+           </div>
+         </div>
+       </div>
+       <div class="exit">
+          <figure>
+               <img class="notification_image" src="../Public/img/exit.png" alt="Cerrar sesión" title="Cerrar Sesion">
+          </figure>
+      </div>
+        <!--<div class="ot">
+          Notificaciones
+        </div>-->
+      </div>
+    <div id="body">
+      Hola Como estas
+    </div>
+    <div class="body2">
+    </div>
+  </body>
+  <script src="../Public/js/modal.js"></script>
+</html>
