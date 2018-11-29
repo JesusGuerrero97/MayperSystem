be385a6cda162c6f14fd14e2a84f182f662033f5
JesusGuerrero97 <32663331+JesusGuerrero97@users.noreply.github.com>
2018-11-29 16:34:01 -0700

    Update inventario.php

--- modelo/inventario.php
+++ modelo/inventario.php
@@ -3,7 +3,7 @@
   function get_productos()
   {
       $con = conectar();
-      $query='select productos.Serie,productos.`Modelo`,productos.`Clave`,categorias.`Nombre_categorias`, status.`Nombre_status` FROM productos INNER JOIN categorias ON productos.`Id_categoria`=categorias.`Id_categoria`INNER JOIN status ON status.`Id_status`= productos.`Id_status`;';
+      $query='select productos.`Modelo`,productos.`Clave`,categorias.`Nombre_categorias`, status.`Nombre_status` FROM productos INNER JOIN categorias ON productos.`Id_categoria`=categorias.`Id_categoria`INNER JOIN status ON status.`Id_status`= productos.`Id_status`;';
       if($con)
       {
         $result = $con->query($query);
@@ -13,6 +13,21 @@
         return null;
       }
   }
+
+  function get_productosAlm()
+  {
+      $con = conectar();
+      $query='select sucursales.`Nombre_sucursal`,productos.Serie,productos.`Modelo`,productos.`Clave`,categorias.`Nombre_categorias`, status.`Nombre_status` FROM productos INNER JOIN categorias ON productos.`Id_categoria`=categorias.`Id_categoria`INNER JOIN STATUS ON status.`Id_status`= productos.`Id_status` INNER JOIN sucursales ON sucursales.`Id_sucursal` = productos.`Id_sucursal`;';
+      if($con)
+      {
+        $result = $con->query($query);
+        return $result;
+      }
+      else {
+        return null;
+      }
+  }
+
   function get_solicitudesVentas()
   {
     $con = conectar();
